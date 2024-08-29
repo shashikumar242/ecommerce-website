a3e03a40c2d70f3e8c0b3032cf05d8f979fbe5af
shashikumar242 <shashikumar.g.242@gmail.com>
2024-08-29 22:01:34 +0530

    fix: rating file issue

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// import Rating from "./Sidebar/rating";
 import Rating from "./Sidebar/rating";
 
 const reviwtitle = "Add a Review";
--- src/pages/Home/CategoryShowCase.jsx
+++ src/pages/Home/CategoryShowCase.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+// import Rating from "../../components/Sidebar/rating";
 import Rating from "../../components/Sidebar/rating";
 
 const title = "Our Products";
-
 
 const ProductData = [
     {
